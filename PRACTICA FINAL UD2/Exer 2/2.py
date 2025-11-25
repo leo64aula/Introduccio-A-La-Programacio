e62e1b5e9ecb5a42897d20b8e06c65772f3a08e8
Leonardo Muñoz Lloret <leo64aula@gmail.com>
2025-11-25 19:59:01 +0100

    UD2exer2

---
+++ PRACTICA FINAL UD2/Exer 2/2.py
@@ -0,0 +1,27 @@
+# Programa amb errors: classifica alumnes per edat i assistència
+print("Classificador d'alumnes per edat i assistència")
+
+n = int(input("Quants alumnes vols processar? "))
+i = 1
+
+while i < n:
+    nom = str(input("Nom: "))
+    edat = int(input("Edat: "))
+    assist = str(input("Assistència (S/N): ")).lower()
+
+    if edat < 12:
+        categoria = "infantil"
+    elif edat >= 12 and edat <= 18:
+        categoria = "adolescent"
+    elif edat >= 18 and edat <= 65:
+        categoria = "adult"
+    else:
+        categoria = "jubilat"
+
+    if assist == "s" or "si":
+        estat = "present"
+    if assist == "n" or "no":
+        estat = "absent"
+
+    print(nom, "-", categoria, "-", estat)
+    i = i + 0
