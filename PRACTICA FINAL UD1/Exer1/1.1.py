2639311bc4715fe6ddb451843dabe0329ce7b58e
Leonardo Muñoz Lloret <leo64aula@gmail.com>
2025-11-25 23:09:07 +0100

    Cambios

--- PRACTICA FINAL UD1/Exer1/1.1.py
+++ PRACTICA FINAL UD1/Exer1/1.1.py
@@ -1,8 +1,8 @@
-print("="* 50)
-print ("EJERCICIO 1.1 - CALCULADORA SIMPLE")
-print("="* 50)
+print("=" * 50)
+print("EJERCICIO 1.1 - CALCULADORA SIMPLE")
+print("=" * 50)
 num1 = int(input("Introduce un número entero: "))
-num2 = float(input("Introduce un número decimal"))
+num2 = float(input("Introduce un número decimal:"))
 suma = num1 + num2
 resta = num1 - num2
 multiplicacion = num1 * num2
@@ -16,5 +16,6 @@ print(f"Multiplicación: {num1} * {num2} = {multiplicacion}")
 print(f"División: {num1} / {num2} = {division}")
 print(f"División entera: {num1} // {num2} = {division_entera}")
 print(f"Módulo: {num1} % {num2} = {modulo}")
-mensaje = "El resultado de sumar " + str(num1) + " y " + str(num2) + " es " + str(suma)
+mensaje = "El resultado de sumar " + \
+    str(num1) + " y " + str(num2) + " es " + str(suma)
 print(f"\nConcatenación: {mensaje}")
