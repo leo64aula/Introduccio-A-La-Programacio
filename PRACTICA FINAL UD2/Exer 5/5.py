8872b825b8dc59ead658b4bffe01e61971c12526
Leo <leo64.aula@gmail.com>
2025-11-26 13:10:06 +0100

    elimnacioexer5

--- PRACTICA FINAL UD2/Exer 5/5.py
+++
@@ -1,7 +0,0 @@
-num = int(input("Introdueix un número: "))
-count = 0
-while num != 0:
-    count += 1
-    num = int(input("Introdueix un altre número: "))
-print(f"S'han introduït {count} números positius.")
-#preguntar a alberto
