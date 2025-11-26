14267e04575b7fd6f4946a92d2c98ad240bd6890
Leonardo Muñoz Lloret <leo64aula@gmail.com>
2025-11-26 19:23:24 +0100

    Exer 9

--- PRACTICA FINAL UD2/Exer 9/9.py
+++ PRACTICA FINAL UD2/Exer 9/9.py
@@ -1,1 +1,10 @@
-#hola
+elements= ["poma", "pera", "taronja", "platan"]
+seleccio= None
+try:
+    pos= int(input("Introdueix una posició del 0 al 3: "))
+    seleccio= elements[pos]
+    print(f"L'element en la posicio {pos} es: {seleccio}")
+except ValueError:
+    print("Introdueix un numero enter")
+except IndexError:
+    print("La posició no existeix en la llista")
