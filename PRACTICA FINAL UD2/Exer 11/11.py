0290b0f08faa8205835c22de9fd425f4bd675846
Leonardo Muñoz Lloret <leo64aula@gmail.com>
2025-11-26 21:55:08 +0100

    WOW

--- PRACTICA FINAL UD2/Exer 11/11.py
+++ PRACTICA FINAL UD2/Exer 11/11.py
@@ -1,1 +1,22 @@
-#hola
+import logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s', 
+    filename= 'operacions.log',
+    filemode='w'
+)
+try:
+    num1= int(input("Disme el primer numero: "))
+    num2= int(input("Disme el segon numero: "))
+    suma= num1 + num2
+    resta= num1 - num2
+    multiplicacio= num1 * num2 
+    divisio= num1 / num2
+    print(f"El resultat de la suma de {num1} + {num2} es igual a {suma}")
+    print(f"El resultat de la resta de {num1} + {num2} es igual a {resta}")
+    print(f"El resultat de la multiplicacio de {num1} + {num2} es igual a {multiplicacio}")
+    print(f"El resultat de la divisio de {num1} + {num2} es igual a {divisio}")
+except ZeroDivisionError:
+    print("El numero 0 no es pot dividir, proba un altre")
+except ValueError:
+    print("Soles pots ficar numeros enters")
--- PRACTICA FINAL UD2/Exer 9/9.py
+++ PRACTICA FINAL UD2/Exer 9/9.py
@@ -1,7 +1,7 @@
 elements= ["poma", "pera", "taronja", "platan"]
 seleccio= None
 try:
-    pos= int(input("Introdueix una posició del 0 al 3: "))
+    pos= int(input("Introdueix una posició del 0 al 3 y te dire a quina fruta correspon: "))
     seleccio= elements[pos]
     print(f"L'element en la posicio {pos} es: {seleccio}")
 except ValueError:
@@ -10,4 +10,6 @@ except IndexError:
     print("La posició no existeix en la llista")
 finally:
     print("Intent Completat")
+    print(f"La longitud de la llista es: {len(elements)}")
     seleccio= None
+    print("La variable seleccio a segut reiniciada a None")
