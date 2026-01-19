f2cd070c5d860ca5a4d586e79320e9a729dbf544
Leo <leo64.aula@gmail.com>
2026-01-19 12:05:24 +0100

    Practica Ud4

---
+++ PRACTICA FINAL UD4/Exer1/Exer1.4/1.4.py
@@ -0,0 +1,9 @@
+import numpy as py
+
+vector = py.array([[[1,2],
+                   [3,4]],
+                  [[5,6],
+                   [7,8]],
+                  [[9,10],
+                   [11,12]]])
+print (vector)
